46b318105a7706916c22622c4cb06a9efe6f8bb4
Alexander Ji <alexji@uchicago.edu>
2025-06-09 09:47:50 -0500

    Imported PayneEchelle directly in from LESSPayne

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TelluricPayne',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # dependencies here
+    author='Alexander Ji',
+    description='Telluric Payne',
+    url='https://github.com/alexji/TelluricPayne',
+)
